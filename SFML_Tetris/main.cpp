4ad3744e77e644646bbd1e342bcee28819833d27
my-rose <101009342+my-rose@users.noreply.github.com>
2022-03-28 16:57:00 +0200

    Initial commit

---
+++ SFML_Tetris/main.cpp
@@ -0,0 +1,30 @@
+#include <SFML\Graphics.hpp>
+
+using namespace sf;
+
+int main()
+{
+	// Объект, который, собственно, является главным окном приложения
+	RenderWindow window(VideoMode(200, 200), "SFML Works!");
+
+	// Главный цикл приложения. Выполняется, пока открыто окно
+	while (window.isOpen())
+	{
+		// Обрабатываем очередь событий в цикле
+		Event event;
+		while (window.pollEvent(event))
+		{
+			// Пользователь нажал на «крестик» и хочет закрыть окно?
+			if (event.type == Event::Closed)
+				// тогда закрываем его
+				window.close();
+		}
+		// Задаем цвет фона
+		window.clear(Color(250, 220, 100, 0));
+
+		// Отрисовка окна	
+		window.display();
+	}
+
+	return 0;
+}
